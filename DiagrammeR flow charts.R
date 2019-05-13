e145c8ecef59bdabe1efc2e67f375f6298de3992
Rich Tyler <rich.tyler.2018@hotmail.com>
2019-05-13 12:18:43 +0100

    Update flow diagram

--- DiagrammeR flow charts.R
+++ DiagrammeR flow charts.R
@@ -4,38 +4,14 @@ library(readr)
 library(DiagrammeR)
 library(tidyverse)
 
-data_1 <- data.frame(Outcome = c("Died", "Did not die", "Fell off", "Survived"), Number = c(41,80,91,102))
-
-label_1 <- paste0("Chocoloca - ", format(1000, big.mark = ","))
-
-grViz("digraph {
-      
-      graph[layout = dot]
-      
-      # default style (can be overridden by individual cells later)
-      node [shape = rectangle, fontsize = 8, fontname = Verdana]
-      
-      # Create your labels
-      node1 [label = '@@1'] 
-      node2 [label = '@@2']
-      process [label =  'Process \n Data']
-      statistical [label = 'Statistical \n Analysis']
-      results [label= 'Results']
-      
-      # edge definitions with the node IDs
-      # Using {} shows two origins going into process
-      {node1 node2}  -> process -> statistical -> results
-      node2 -> results
-      
-      }
-    [1]: paste0(data_1$Outcome[1], ' - ', data_1$Number[1])
-    [2]: paste0(data_1$Outcome[2], ' - ', data_1$Number[2])
-    ")
-
 Flow_df <- read_csv("C:/Users/Rich/Downloads/Flow_df.csv") %>% 
   mutate(Flow_label = gsub("Didn't", "Did not", Flow_label)) %>% 
   filter(!(Node %in% c(18, 21, 25, 27))) %>% 
-  mutate(Node = row_number())
+  mutate(Node = row_number()) %>% 
+  mutate(Flow_label = ifelse(Node_level == 4, gsub("\r\n", " ", Flow_label), Flow_label)) %>% 
+  mutate(Flow_label = gsub("   ", " ", Flow_label))
+
+Flow_df$Flow_label
 
 grViz("digraph {
       
@@ -55,27 +31,49 @@ node [shape = rectangle, fontsize = 8, fontname = Verdana]
 9 [label = '@@9']
 10 [label = '@@10']
 11 [label = '@@11']
-12 [label = '@@12']
-13 [label = '@@13']
-14 [label = '@@14']
-15 [label = '@@15']
-16 [label = '@@16']
-17 [label = '@@17']
-18 [label = '@@18']
-19 [label = '@@19']
-20 [label = '@@20']
-21 [label = '@@21']
-22 [label = '@@22']
-23 [label = '@@23']
+12 [label = '@@12', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+13 [label = '@@13', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+14 [label = '@@14', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+15 [label = '@@15', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+16 [label = '@@16', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+17 [label = '@@17', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+18 [label = '@@18', style=filled, fillcolor = '#91cf60', height = 0.25, width = 1.8]
+19 [label = '@@19', style=filled, fillcolor = '#91cf60', height = 0.25, width = 2.4]
+20 [label = '@@20', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+21 [label = '@@21', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+22 [label = '@@22', style=filled, fillcolor = '#e7e7e7', height = 0.25, width = 1.8]
+23 [label = '@@23', style=filled, fillcolor = '#91cf60', height = 0.25, width = 2.4]
 
 1 -> {2 3}
 2 -> {4 5 6 7}
 3 -> {8 9 10 11}
-4 -> {12 13 14 15 16 17}
-6 -> {18 19}
-8 -> {20 21 22}
+4 -> 12
+
+subgraph cluster_early_stillopen {
+graph [ranksep = 0.1, style = dotted]
+12 -> 13 -> 14 -> 15 -> 16 -> 17 [style = invis, length = 0.5]
+}
+
+6 -> 18
+subgraph cluster_resolved_stillopen {
+graph [ranksep = 0.1, style = dotted]
+18 -> 19 [style = invis]
+}
+
+8 -> 20
+subgraph cluster_early_closed {
+graph [ranksep = 0.1, style = dotted]
+20 -> 21 -> 22 [style = invis]
+}
+
 10 -> 23
-    }
+subgraph cluster_resolved_closed{
+graph [ranksep = 0.1, style = dotted]
+23
+}
+
+}
+    
 [1]: paste0(Flow_df$Flow_label[1])
 [2]: paste0(Flow_df$Flow_label[2])
 [3]: paste0(Flow_df$Flow_label[3])
